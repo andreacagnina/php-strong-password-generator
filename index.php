a12dfacfeb845c8359cdfd6cd73e331a7fc2f402
andreacagnina <amirkal@libero.it>
2024-09-09 00:45:37 +0200

    [feat] layout

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
 </head>
 
 <body>
+    <main>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-12">
+                    <form action="./index.php" method="get">
+                        <div class="col-4 my-3">
+                            <label for="">Scegli una lunghezza per la tua password</label>
+                            <input type="number" name="lunghezza" id="lunghezza" min="5" max="10" class="form-control" placeholder="Inserisci un numero">
+                        </div>
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-primary">Genera</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
 
 </body>
 
